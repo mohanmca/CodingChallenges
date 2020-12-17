36672d6c4005f6eff78cfef403822b6633328098
Mohan Narayanaswamy <mohanmca@gmail.com>
2020-12-17 22:04:58 +0800

    LeetCode

--- src/main/java/leedcode/array/SingleNumber.java
+++ src/main/java/leedcode/array/SingleNumber.java
@@ -2,7 +2,7 @@ package leedcode.array;
 
 import java.util.Arrays;
 
-class Solution {
+class SingleNumber {
     public static void main(String[] args) {
         System.out.println(singleNumber(new int[]{2, 2, 1}));
         System.out.println(singleNumber(new int[]{4, 1, 2, 1, 2}));
