59905813b75ed9ddae023ebf13a687a0973ffa24
Mohan Narayanaswamy <mohanmca@gmail.com>
2021-01-08 08:13:59 +0800

    Array

---
+++ src/main/java/leedcode/array/SubArratSumEqualsK.java
@@ -0,0 +1,37 @@
+package leedcode.array;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class SubArratSumEqualsK {
+    public static void main(String[] args) {
+        //System.out.println(subarraySum(new int[]{1, 2, 3}, 3));
+        System.out.println(subarraySum(new int[]{1, 2, 1, 2, 1}, 3));
+    }
+
+    public static int subarraySum(int[] nums, int k) {
+        Arrays.sort(nums);
+        System.out.println(Arrays.toString(nums));
+        return subarraySum(nums, k, 0, nums.length - 1);
+    }
+
+    public static int subarraySum(int[] nums, int k, int low, int high) {
+        if (low > high) return 0;
+
+        if (nums[high] - k > 0) {
+            return subarraySum(nums, nums[high] - k, low, high - 1);
+        } else if (nums[high] - k == 0) {
+            return 1 + subarraySum(nums, k, low, high - 1);
+        } else if (nums[high] - k < 0) {
+            return subarraySum(nums, k - nums[high], low, high - 1);
+        } else if (nums[low] - k < 0) {
+            return subarraySum(nums, k - nums[low], low + 1, high);
+        } else if (nums[low] - k > 0) {
+            return 0;
+        }
+        return 1 + subarraySum(nums, k, low + 1, high);
+    }
+
+
+}
--- src/main/java/leedcode/array/ThreeSum.java
+++ src/main/java/leedcode/array/ThreeSum.java
@@ -1,31 +1,46 @@
 package leedcode.array;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
+import java.util.function.Consumer;
+import java.util.stream.Collectors;
 
 public class ThreeSum {
     public static void main(String[] args) {
         Integer[] t = new Integer[0];
-        threeSum(new int[]{-1, 0, 1, 2, -1, -4}).forEach(l -> System.out.println(Arrays.toString(l.toArray(t))));
-        //threeSum(new int[]{-3, -2, -1, 0, 1, 2, 3}).forEach(l -> System.out.println(Arrays.toString(l.toArray(t))));
-        //threeSum(new int[]{-3, -2}).forEach(l -> System.out.println(Arrays.toString(l.toArray(t))));
+        printResult(threeSum(new int[]{-2, 0, 1, 1, 2}));
+        printResult(threeSum(new int[]{-1, 0, 1, 2, -1, -4}));
+        printResult(threeSum(new int[]{0, 0, 0, 0}));
+        printResult(threeSum(new int[]{-3, -2, -1, 0, 1, 2, 3}));
+        printResult(threeSum(new int[]{-3, -2}));
+    }
+
+    private static void printResult(List<List<Integer>> t) {
+        t.forEach(l -> System.out.println(Arrays.toString(l.toArray())));
+        System.out.println("---");
     }
 
     public static List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> lists = new ArrayList<>();
-        if (nums.length < 3) return lists;
+        Set<List<Integer>> lists = new HashSet<List<Integer>>();
+        if (nums.length < 3) return new ArrayList<>(lists);
         Arrays.sort(nums);
-        for (int j = nums.length - 1; j > 1; j--) {
-            int i = 0;
-            while (nums[i] + nums[i + 1] + nums[j] <= 0 && i < j - 1) {
-                if (nums[i] + nums[i + 1] + nums[j] == 0) {
-                    lists.add(Arrays.asList(nums[i], nums[i + 1], nums[j]));
+        for (int j = nums.length - 1; j > 1 && nums[j] >= 0; j--) {
+            int left = 0;
+            int right = j - 1;
+            int target = nums[j] * -1;
+            while (left < right) {
+                if (nums[left] + nums[right] > target) {
+                    right--;
+                } else if (nums[left] + nums[right] < target) {
+                    left++;
+                } else {
+                    lists.add(Arrays.asList(nums[left], nums[right], nums[j]));
+                    left++;
+                    right--;
                 }
-                i++;
             }
+
         }
-        return lists;
+        return new ArrayList<>(lists);
     }
 
 }
